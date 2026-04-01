9fcdf6ff66916ed6ebc49b0231e4bd33ed1b650d
lapinRagnar <lapinragnar@gmail.com>
2026-04-01 21:46:39 +0200

    Structure de base avec une seule tâche

--- a3-todo-list/src/main.rs
+++ a3-todo-list/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// Ligne 1: Définition d'une structure Todo
+struct Todo {
+    title: String,
+    completed: bool,
 }
+
+fn main() {
+    // création d'une instance de Todo
+    let todo = Todo {
+        title: String::from("Apprendre rust"),
+        completed: false,
+    };
+
+    // affichage de la tâche
+    println!("Tache: {}, completée: {}", todo.title, todo.completed);
+}
+
+
