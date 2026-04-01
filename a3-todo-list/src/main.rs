f143a3e961191db585f94c2ddee52de53f566c0c
lapinRagnar <lapinragnar@gmail.com>
2026-04-01 22:00:27 +0200

    Étape 3: Ajout des méthodes impl pour la struct todo

--- a3-todo-list/src/main.rs
+++ a3-todo-list/src/main.rs
@@ -4,15 +4,35 @@ struct Todo {
     completed: bool,
 }
 
+// bloc impl pour les méthodes associées à la structure Todo
+impl Todo {
+    // constructeur new
+    fn new(title: String) -> Todo {
+        Todo {
+            title: title,
+            completed: false, 
+        }
+    }
+
+    // méthode pour marquer la tâche comme complétée
+    fn complete(&mut self) {
+        self.completed = true;
+    }
+}
+
 fn main() {
-    // création d'une instance de Todo
-    let todo = Todo {
-        title: String::from("Apprendre rust"),
-        completed: false,
-    };
+    
+    // utilisation du constructeur pour créer une nouvelle tâche
+    let mut todo = Todo::new(String::from("apprendre rust"));
+
 
     // affichage de la tâche
-    println!("Tache: {}, completée: {}", todo.title, todo.completed);
+    println!("Tache avant: {}, completée: {}", todo.title, todo.completed);
+
+    // appel de la méthode
+    todo.complete();
+
+    println!("Tache après: {}, completée: {}", todo.title, todo.completed);
 }
 
 
